8d904194d18b0cafb03dc03d9d230c475633106c
Matt Lubner <matt@mattlubner.com>
2015-12-10 20:04:44 -0500

    ++jsdocs to manager

--- manager.js
+++ manager.js
@@ -1,8 +1,11 @@
+/**
+ * Instance indexer provides a way to create/retrieve class instances by name.
+ */
+
 'use strict';
 
 /**
- * Instance Manager; Provides a way to create/retrieve class instances by
- * name.
+ * Manager constructor
  * @arg  [String]  name
  * @constructor
  */
@@ -11,12 +14,21 @@ function Manager( name ) {
   Object.defineProperty( this, 'instances', { value: Object.create( null ), writable: true } );
 }
 
+/**
+ * Manager factory function
+ * @return  {Manager}
+ */
 Manager.prototype.create = Manager.create = function () {
   var instance = Object.create( this.prototype || this );
   Manager.apply( instance, arguments );
   return instance;
 };
 
+/**
+ * Add an instance to the managed instance index.
+ * @arg  {Object}  instance
+ * @void
+ */
 Manager.prototype.addInstance = function ( instance ) {
   if ( typeof instance !== 'object' ) {
     throw new Error( 'No instance was passed' );
@@ -27,6 +39,11 @@ Manager.prototype.addInstance = function ( instance ) {
   this.instances[ instance.name ] = instance;
 };
 
+/**
+ * Get an instance from the managed instance index.
+ * @arg  {String}  name
+ * @return  {Object}
+ */
 Manager.prototype.getInstance = function ( name ) {
   if ( typeof name !== 'string' ) {
     throw new Error( 'No instance name provided!' );
@@ -34,6 +51,10 @@ Manager.prototype.getInstance = function ( name ) {
   return this.instances[ name ];
 };
 
+/**
+ * Duuned plugin methods.
+ * @type {Array}
+ */
 Object.defineProperty( Manager, 'duun', {
   value: Object.create( null, {
     methods: {
@@ -45,4 +66,4 @@ Object.defineProperty( Manager, 'duun', {
   } )
 } );
 
-module.exports = exports = Manager;
+module.exports = Manager;
