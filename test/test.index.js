3e49f2f1e2b96b0ba348b6ae656e60651f57f1dc
Matt Lubner <matt@mattlubner.com>
2015-12-10 20:00:57 -0500

    ++tests for index

--- test/test.index.js
+++ test/test.index.js
@@ -13,6 +13,11 @@ describe( 'index', function () {
   var Logger = require( '../logger' );
   var Manager = require( '../manager' );
 
+  var mockConsole = require( './mock.console' );
+  Logger.inject( {
+    console: mockConsole
+  } );
+
   // check if duun instance is properly initiated
   describe( 'duun', function () {
     it( 'should remain uninitialized', function () {
@@ -34,6 +39,20 @@ describe( 'index', function () {
         assert.isFunction( Index[ methodName ] );
       } );
     } );
+    it( 'should proxy log() calls to the console while enabled', function () {
+      mockConsole.resetSpies();
+      var aLogger = Logger.create( 'a logger' );
+      aLogger.log( 'a message to log' );
+      assert( mockConsole.logSpy.calledOnce );
+    } );
+    it( 'should not proxy log() calls to the console while disabled', function () {
+      mockConsole.resetSpies();
+      Logger.consoleDisable();
+      var aLogger = Logger.create( 'a logger' );
+      aLogger.log( 'a message to log' );
+      assert.equal( 0, mockConsole.logSpy.callCount );
+      Logger.consoleEnable();
+    } );
   } );
 
   // check that manager is properly registered
@@ -43,6 +62,15 @@ describe( 'index', function () {
         assert.property( Index, methodName );
         assert.isFunction( Index[ methodName ] );
       } );
+    } );
+    it( 'should store and retrieve duun instances by name', function () {
+      var duun1 = Index.create( 'a duuny duun' );
+      Index.addInstance( duun1 );
+      assert.equal( duun1, Index.getInstance( 'a duuny duun' ) );
+      assert.deepEqual( duun1, Index.getInstance( 'a duuny duun' ) );
+      var duun2 = Index.create( 'another super duuny duun' );
+      assert.isDefined( duun2 );
+      assert.isUndefined( Index.getInstance( 'another super duuny duun' ) );
     } );
   } );
 
