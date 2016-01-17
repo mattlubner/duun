9053c2e9863be05f96c8360570f4e230e761eced
Matt Lubner <matt@mattlubner.com>
2016-01-16 16:43:34 -0800

    refactor: simplified export declaration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Duun = module.exports = exports = require( './duun' );
+var Duun = require( './duun' );
 
 var Logger = require( './logger' );
 Duun.registerCorePlugin( Logger );
@@ -9,3 +9,5 @@ var Manager = require( './manager' );
 Duun.registerCorePlugin( Manager );
 
 delete Duun.registerCorePlugin;
+
+module.exports = Duun;
